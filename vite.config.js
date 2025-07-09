508a78ad8a4edd1c76634cdf808d443bad6ced6e
Afunsho Olamide <abolamide77@gmail.com>
2025-07-09 21:24:37 +0100

    Updated project

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/https://github.com/OlamideAfunsho/react-jobs-website.git/',
   server: {
     proxy: {
       '/api': {
